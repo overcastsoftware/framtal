e89ae3e3b1f5f586c1e719e883b7f9f7a34bb265
Sævar Öfjörð Magnússon <saevar@saevar.is>
2025-05-07 13:40:45 +0000

    Phone number

--- framtal-api/src/login/login.resolver.ts
+++ framtal-api/src/login/login.resolver.ts
@@ -5,6 +5,9 @@ export class LoginResolver {
   @Mutation(() => Boolean)
   login(@Args('phoneNumber') phoneNumber: string): boolean {
     console.log(`Login attempt with phone number: ${phoneNumber}`);
+    if (phoneNumber !== '7728391') {
+      throw new Error('Not a valid phone number');
+    }
     return true; // Always return success for demonstration purposes
   }
 }

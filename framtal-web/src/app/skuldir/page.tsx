7e106764331821132665167ad2bdba8700aeacb3
Sævar Öfjörð Magnússon <saevar@saevar.is>
2025-05-07 14:26:04 +0000

    Update paths

---
+++ framtal-web/src/app/skuldir/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useParams } from 'next/navigation';
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+
+export default function BlogPost() {
+  
+
+  return (
+    <Layout>
+Skuldir
+    </Layout>
+  );
+}

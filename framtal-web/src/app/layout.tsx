eb8243b873a9b97f5917ab0f60be7b635a4104e9
Daniel Ingolfsson <daniel@overcast.is>
2025-05-08 11:35:40 +0000

    Fonts

--- framtal-web/src/app/layout.tsx
+++ framtal-web/src/app/layout.tsx
@@ -14,6 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </head>
       <body>
         <ApolloProvider>{children}</ApolloProvider>
       </body>

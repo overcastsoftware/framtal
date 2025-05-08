0ece34e44b28b5edd9c888f41c3b8348cfff55fe
Sævar Öfjörð Magnússon <saevar@saevar.is>
2025-05-08 12:00:42 +0000

    Crossorigin

--- framtal-web/src/app/layout.tsx
+++ framtal-web/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
           href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
